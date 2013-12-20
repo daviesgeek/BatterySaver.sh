ba791263dc3c8cbf4a582d37e87216444bb96c99
Matthew Davies <daviesgeek@icloud.com>
2013-12-19 19:30:19 -0800

    Moved battery.sh to BatterySaver.sh

---
+++ BatterySaver.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+  source 'global.sh'
+
+  #If there isn't a log file, assume that this is the first run
+  if [[ ! -f $configfile ]] ; then
+  	source 'first-run.sh' #First run script
+  else
+    readSettings
+  fi
+
+  if [ $os == '10' ]; then #If this is running on OS X
+  	if [[ $osDot > '8' ]] && [[ $notify ]]; then
+  		source 'ml-notify.sh'
+  	else
+  		source 'legacy-notify.sh'
+  	fi
+  fi
+
+  #Here's where the actual fun happens
+  if [ $plugged == 'No' ] && [[ "$charge" < 40 ]]; then
+  	echo 'not plugged in'
+  fi
+
+die
+
+## add growlnotify support
+## https://github.com/indirect/growlnotify
+
+#while true; do
+  #sleep 300
+#done
